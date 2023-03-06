f0d39c87d8d2a32b4d6defd539995bff7958a0f9
vishnu4reddy <vreddy@msystechnologies.com>
2023-03-06 17:40:56 +0530

    done

--- Tests/test_login.py
+++ Tests/test_login.py
@@ -1,12 +1,12 @@
 from Page_Object.login_page_po import LoginPage
-from playwright.sync_api import sync_playwright
+# from playwright.sync_api import sync_playwright
 from Data import config
 Email = config.user_git
 password = config.git_pass
 
 
-def test_check_login(page4):
+def test_check_login(page):
 
-    login_page = LoginPage(page4)
+    login_page = LoginPage(page)
     login_page.navigate()
     login_page.login(Email, password)
