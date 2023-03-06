f41119ccb1786b9c01258643619d76f550181103
vishnu4reddy <vreddy@msystechnologies.com>
2023-03-06 16:48:43 +0530

    done

--- conftest.py
+++ conftest.py
@@ -1,3 +1,15 @@
+# from .utils import utils as utils_object
+# from .utils import Utils
+# from playwright.sync_api import (
+#     Browser,
+#     BrowserContext,
+#     BrowserType,
+#     Page,
+#     Playwright,
+#     Selectors,
+#     sync_playwright,
+# )
+from typing import Dict, Generator
 import pytest
 from playwright.sync_api import sync_playwright
 
@@ -6,7 +18,7 @@ from playwright.sync_api import sync_playwright
 def page4(pytestconfig):
     with sync_playwright() as p:
         # browser = p.chromium.launch(headless=False)
-        browser = playwright.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=True)
         print(pytestconfig.getoption("--headed"))
         # print(pytestconfig)
         # context = browser.new_context()
@@ -27,73 +39,55 @@ def page4(pytestconfig):
 # limitations under the License.
 
 
-from typing import Dict, Generator
-
-import pytest
-
-from playwright.sync_api import (
-    Browser,
-    BrowserContext,
-    BrowserType,
-    Page,
-    Playwright,
-    Selectors,
-    sync_playwright,
-)
-
-from .utils import Utils
-from .utils import utils as utils_object
+# @pytest.fixture
+# def utils() -> Generator[Utils, None, None]:
+#     yield utils_object
 
 
-@pytest.fixture
-def utils() -> Generator[Utils, None, None]:
-    yield utils_object
+# @pytest.fixture(scope="session")
+# def playwright() -> Generator[Playwright, None, None]:
+#     with sync_playwright() as p:
+#         yield p
 
 
-@pytest.fixture(scope="session")
-def playwright() -> Generator[Playwright, None, None]:
-    with sync_playwright() as p:
-        yield p
+# @pytest.fixture(scope="session")
+# def browser_type(
+#     playwright: Playwright, browser_name: str
+# ) -> Generator[BrowserType, None, None]:
+#     browser_type = None
+#     if browser_name == "chromium":
+#         browser_type = playwright.chromium
+#     elif browser_name == "firefox":
+#         browser_type = playwright.firefox
+#     elif browser_name == "webkit":
+#         browser_type = playwright.webkit
+#     assert browser_type
+#     yield browser_type
 
 
-@pytest.fixture(scope="session")
-def browser_type(
-    playwright: Playwright, browser_name: str
-) -> Generator[BrowserType, None, None]:
-    browser_type = None
-    if browser_name == "chromium":
-        browser_type = playwright.chromium
-    elif browser_name == "firefox":
-        browser_type = playwright.firefox
-    elif browser_name == "webkit":
-        browser_type = playwright.webkit
-    assert browser_type
-    yield browser_type
+# @pytest.fixture(scope="session")
+# def browser(
+#     browser_type: BrowserType, launch_arguments: Dict
+# ) -> Generator[Browser, None, None]:
+#     browser = browser_type.launch(**launch_arguments)
+#     yield browser
+#     browser.close()
 
 
-@pytest.fixture(scope="session")
-def browser(
-    browser_type: BrowserType, launch_arguments: Dict
-) -> Generator[Browser, None, None]:
-    browser = browser_type.launch(**launch_arguments)
-    yield browser
-    browser.close()
+# @pytest.fixture
+# def context(browser: Browser) -> Generator[BrowserContext, None, None]:
+#     context = browser.new_context()
+#     yield context
+#     context.close()
 
 
-@pytest.fixture
-def context(browser: Browser) -> Generator[BrowserContext, None, None]:
-    context = browser.new_context()
-    yield context
-    context.close()
+# @pytest.fixture
+# def page(context: BrowserContext) -> Generator[Page, None, None]:
+#     page = context.new_page()
+#     yield page
+#     page.close()
 
 
-@pytest.fixture
-def page(context: BrowserContext) -> Generator[Page, None, None]:
-    page = context.new_page()
-    yield page
-    page.close()
-
-
-@pytest.fixture(scope="session")
-def selectors(playwright: Playwright) -> Selectors:
-    return playwright.selectors
+# @pytest.fixture(scope="session")
+# def selectors(playwright: Playwright) -> Selectors:
+#     return playwright.selectors
