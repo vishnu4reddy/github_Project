7ce08d495502055a0be9d5607ea781a26d789c55
vishnu4reddy <vreddy@msystechnologies.com>
2023-03-07 13:14:45 +0530

    done

--- conftest.py
+++ conftest.py
@@ -18,7 +18,7 @@ from playwright.sync_api import sync_playwright
 def page4(pytestconfig):
     with sync_playwright() as p:
         # browser = p.chromium.launch(headless=False)
-        # browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=True)
         browser = p.chromium.launch(channel="chrome")
         # print(pytestconfig.getoption("--headed"))
         # print(pytestconfig)
